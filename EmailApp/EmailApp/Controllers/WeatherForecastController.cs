ddb6ae17f084bc0598cfa3d46453f2e848555d10
Marinko <marinkospasojevic@gmail.com>
2019-12-30 09:50:38 +0100

    Sending Email with Attachments ASP.NET Core source code

---
+++ EmailApp/EmailApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmailService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MailApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly IEmailSender _emailSender;
+
+        public WeatherForecastController(IEmailSender emailSender)
+        {
+            _emailSender = emailSender;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecast>> Get()
+        {
+            var rng = new Random();
+
+            var message = new Message(new string[] { "codemazetest@mailinator.com" }, "Test email async", "This is the content from our async email.", null);
+            await _emailSender.SendEmailAsync(message);
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpPost]
+        public async Task<IEnumerable<WeatherForecast>> Post()
+        {
+            var rng = new Random();
+
+            var files = Request.Form.Files.Any() ? Request.Form.Files : new FormFileCollection();
+
+            var message = new Message(new string[] { "codemazetest@mailinator.com" }, "Test mail with Attachments", "This is the content from our mail with attachments.", files);
+            await _emailSender.SendEmailAsync(message);
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # email-attachments-aspnet-core
+## https://code-maze.com/send-email-with-attachments-aspnetcore
 This repo contains the source code for "How to Send Email with Attachments in ASP.NET Core" article on Code Maze
